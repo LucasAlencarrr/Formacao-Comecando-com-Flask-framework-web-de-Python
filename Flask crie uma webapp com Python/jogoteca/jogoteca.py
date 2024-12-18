df9bd40eff216aee5c7b421a153d24b65e36496e
Lucas Alencar <lucasalencar.br@gmail.com>
2024-12-18 17:21:46 -0400

    Adicionando 3 jogos e alterando o Template HTML com 3 novas colunas (Nome, Categoria, Console)

--- Flask crie uma webapp com Python/jogoteca/jogoteca.py
+++ Flask crie uma webapp com Python/jogoteca/jogoteca.py
@@ -11,8 +11,9 @@ app = Flask(__name__)
 @app.route('/inicio')
 def ola():
     jogo1 = Jogo("Tetris", "Puzzle", "Atari")
-    jogo2 = Jogo("God of War", "Rack n Slash", "PS2") 
-    lista = [jogo1, jogo2]
+    jogo2 = Jogo("God of War", "Rack n Slash", "PS2")
+    jogo3 = Jogo("Mortal Kombat", "Luta", "PS2") 
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo = "Jogos", jogos = lista)
 
 
