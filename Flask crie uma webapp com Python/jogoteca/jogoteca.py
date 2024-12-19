94ed1883761c2750247e1c815a5ce9b2fc6e25ed
Lucas Alencar <lucasalencar.br@gmail.com>
2024-12-19 00:58:02 -0400

    Adicionando arquivo css Bootstrap e relacionando com templates.

--- Flask crie uma webapp com Python/jogoteca/jogoteca.py
+++ Flask crie uma webapp com Python/jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -13,23 +13,21 @@ lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    
-    
+@app.route('/')
+def index():
     return render_template('lista.html', titulo = "Jogos", jogos = lista)
 
 @app.route("/novo")
 def novo():
     return render_template("novo.html", titulo = "Novo Jogo")
 
-@app.route('/criar')
+@app.route('/criar', methods = ['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo = "Jogos", jogos = lista)
+    return redirect('/')
 
-app.run()
+app.run(debug = True)
